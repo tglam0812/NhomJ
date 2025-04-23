27564bfa4841219b89c179b7c67f0d3a7c6017e6
tglam0812 <trangialam2004@gmail.com>
2025-04-24 06:46:23 +0700

    fix conflict web

--- Doan_BE2/routes/web.php
+++ Doan_BE2/routes/web.php
@@ -1,21 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\UserController;
-use Illuminate\Support\Facades\Controller;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CrudProductsController;
+use App\Http\Controllers\InformationController;
+use App\Http\Controllers\HomeController;
 
 // Route::get('login', [LoginController::class, 'login'])->name('login');
 
+// Route::get('/', function () {
+//     return view('index');
+// });
+Route::get('/', [HomeController::class, 'page'])->name('home');
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login'])->name('login');
-
-Route::get('/', function () {
-    return view('index');
-});
 
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register'])->name('register');
@@ -29,5 +29,16 @@ Route::prefix('products')->group(function () {
     Route::post('/{id}/edit', [CrudProductsController::class, 'postUpdateProduct'])->name('products.postUpdateProduct');
     Route::get('/{id}', [CrudProductsController::class, 'readProduct'])->name('products.readProduct');
     Route::delete('/{id}', [CrudProductsController::class, 'deleteProduct'])->name('products.deleteProduct');
-    
+});
+
+// Logout
+Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+// Information (auth middleware applied)
+Route::group(['prefix' => 'auth', 'middleware' => ['auth']], function() {
+    Route::get('/info', [InformationController::class, 'info'])->name('auth.info');
+    Route::patch('/info/{user_id}', [InformationController::class, 'update'])->name('info.update');
+    Route::get('/resetpassword', [InformationController::class, 'showresetpassword'])->name('auth.showresetpassword');
+    Route::patch('/resetpassword/{user_id}', [InformationController::class, 'resetpassword'])->name('info.resetpassword');
 });
