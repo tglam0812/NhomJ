5e1de28536293655c3fdefefa12fbee5dff0654b
vyva2004 <vyva2004@gmail.com>
2025-04-23 18:05:11 +0700

    sua web

--- Doan_BE2/routes/web.php
+++ Doan_BE2/routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\UserController;
-use Illuminate\Support\Facades\Controller;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\CrudProductsController;
 use App\Http\Controllers\InformationController;
 use App\Http\Controllers\HomeController;
-
 
 // Route::get('login', [LoginController::class, 'login'])->name('login');
 
@@ -22,12 +20,23 @@ Route::post('login', [LoginController::class, 'login'])->name('login');
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register'])->name('register');
 
-//Logout
+// Products
+Route::prefix('products')->group(function () {
+    Route::get('/', [CrudProductsController::class, 'listProduct'])->name('products.list');
+    Route::get('/create', [CrudProductsController::class, 'createProduct'])->name('products.createProduct');
+    Route::post('/create', [CrudProductsController::class, 'postProduct'])->name('products.postProduct');
+    Route::get('/{id}/edit', [CrudProductsController::class, 'updateProduct'])->name('products.updateProduct');
+    Route::post('/{id}/edit', [CrudProductsController::class, 'postUpdateProduct'])->name('products.postUpdateProduct');
+    Route::get('/{id}', [CrudProductsController::class, 'readProduct'])->name('products.readProduct');
+    Route::delete('/{id}', [CrudProductsController::class, 'deleteProduct'])->name('products.deleteProduct');
+});
+
+// Logout
 Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
-//infomation
-Route::group(['prefix' => 'auth', 'middleware' => ['auth']], function(){
+// Information (auth middleware applied)
+Route::group(['prefix' => 'auth', 'middleware' => ['auth']], function() {
     Route::get('/info', [InformationController::class, 'info'])->name('auth.info');
     Route::patch('/info/{user_id}', [InformationController::class, 'update'])->name('info.update');
     Route::get('/resetpassword', [InformationController::class, 'showresetpassword'])->name('auth.showresetpassword');
