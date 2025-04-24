201d9cbf656529e21ffe4c370efe43b8d76037ca
tglam0812 <trangialam2004@gmail.com>
2025-04-24 12:45:43 +0700

    update

--- Doan_BE2/routes/web.php
+++ Doan_BE2/routes/web.php
@@ -3,18 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
-<<<<<<< HEAD
 use App\Http\Controllers\CrudProductsController;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\InformationController;
 use App\Http\Controllers\HomeController;
-
->>>>>>> TrieuVy
-=======
-use App\Http\Controllers\InformationController;
-use App\Http\Controllers\HomeController;
->>>>>>> TranGiaLam
 
 // Route::get('login', [LoginController::class, 'login'])->name('login');
 
@@ -29,7 +20,6 @@ Route::post('login', [LoginController::class, 'login'])->name('login');
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register'])->name('register');
 
-<<<<<<< HEAD
 // Products
 Route::prefix('products')->group(function () {
     Route::get('/', [CrudProductsController::class, 'listProduct'])->name('products.list');
@@ -52,16 +42,3 @@ Route::group(['prefix' => 'auth', 'middleware' => ['auth']], function() {
     Route::get('/resetpassword', [InformationController::class, 'showresetpassword'])->name('auth.showresetpassword');
     Route::patch('/resetpassword/{user_id}', [InformationController::class, 'resetpassword'])->name('info.resetpassword');
 });
-=======
-//Logout
-Route::get('logout', [LoginController::class, 'logout'])->name('logout');
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-
-//infomation
-Route::group(['prefix' => 'auth', 'middleware' => ['auth']], function(){
-    Route::get('/info', [InformationController::class, 'info'])->name('auth.info');
-    Route::patch('/info/{user_id}', [InformationController::class, 'update'])->name('info.update');
-    Route::get('/resetpassword', [InformationController::class, 'showresetpassword'])->name('auth.showresetpassword');
-    Route::patch('/resetpassword/{user_id}', [InformationController::class, 'resetpassword'])->name('info.resetpassword');
-});
->>>>>>> TrieuVy
